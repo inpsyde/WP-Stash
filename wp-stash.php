764721db2cb3b168111d65e7ce6fb68dccf29d2c
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-09-04 15:13:08 +0200

    Simplify bootstrap

--- wp-stash.php
+++ wp-stash.php
@@ -12,34 +12,24 @@
 
 namespace Inpsyde\WpStash;
 
-/**
- * Spawn a little helper to put admin notices on the WP Admin panel.
- *
- * @param $message
- */
-$admin_notice = function ( $message ) {
+if (! class_exists(__NAMESPACE__ . '\\WpStash')) {
+    if (is_readable(__DIR__ . '/vendor/autoload.php')) {
+        /** @noinspection PhpIncludeInspection */
+        require_once __DIR__ . '/vendor/autoload.php';
+    } else {
+        foreach (['admin_notices', 'network_admin_notices'] as $hook) {
+            add_action($hook, function () {
 
-	foreach ( [ 'admin_notices', 'network_admin_notices' ] as $hook ) {
-		add_action( $hook, function () use ( $message ) {
+                $message = __(
+                    'Could not find a working autoloader for WP Stash',
+                    'inpsyde-multisite-menu'
+                );
+                $class   = 'notice notice-error';
+                printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
+            });
+        }
 
-			$class = 'notice notice-error';
-			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-		} );
-	}
-
-};
-
-if ( ! class_exists( __NAMESPACE__ . '\\WpStash' ) ) {
-
-	if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-		/** @noinspection PhpIncludeInspection */
-		require_once __DIR__ . '/vendor/autoload.php';
-
-	} else {
-		$admin_notice( __( 'Could not find a working autoloader for WP Stash',
-			'inpsyde-multisite-menu' ) );
-
-		return;
-	}
+        return;
+    }
 }
-( new WpStash( __DIR__ . '/object-cache.php' ) )->init();
+(new WpStash(__DIR__ . '/object-cache.php'))->init();
