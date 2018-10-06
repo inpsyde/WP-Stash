19ac974da04e9e05da4f41372c9d064e4866287d
Viktor Szépe <viktor@szepe.net>
2018-10-06 20:59:46 +0200

    Use a symlink on non-Windows OS
    `object-cache.php` is meant to be a symbolic link only.

--- inc/WpStash.php
+++ inc/WpStash.php
@@ -143,7 +143,11 @@ class WpStash
 
         $target = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . $this->dropinName;
         if (! file_exists($target)) {
-            copy($this->dropinPath, $target);
+            if ('WIN' === strtoupper(substr(PHP_OS, 0, 3))) {
+                copy($this->dropinPath, $target);
+            } else {
+                symlink($target, $this->dropinPath);
+            }
         }
 
         if (is_admin()) {
