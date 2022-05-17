ff8d51a1019f3ee93a2b7b4858dfab3867598a43
David Naber <kontakt@dnaber.de>
2022-05-17 09:53:44 +0200

    Raise dependencies versions to be PHP 8 compatible #21
    - raise tedivm/stash to ^0.17.1
    
    dev dependencies:
    - raise phpunit/phpunit to ^9.5
    - raise inpsyde/php-coding-standards to ^1.0
    - raise squizlabs/php_codesniffer to ^3.6
    - adapt PHPUnit ::assertContains() to ::assertStringContainsString()

--- tests/PHPUnit/Unit/Generator/CacheKeyGeneratorTest.php
+++ tests/PHPUnit/Unit/Generator/CacheKeyGeneratorTest.php
@@ -14,6 +14,6 @@ class CacheKeyGeneratorTest extends AbstractUnitTestcase
 
         $expectedKey = 'foo';
 
-        static::assertContains($expectedKey, $testee->create($expectedKey));
+        static::assertStringContainsString($expectedKey, $testee->create($expectedKey));
     }
 }
